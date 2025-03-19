845e735aac2e3565ecb36d9559fbe1c78f82db5f
oscarloma <oscaar_2003@hotmail.com>
2025-03-19 09:13:21 -0500

    add polly

--- NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
+++ NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
@@ -1,6 +1,9 @@
 using NSE.WebApp.MVC.Extensions;
 using NSE.WebApp.MVC.Services;
 using NSE.WebApp.MVC.Services.Handlers;
+using Polly;
+using Polly.Extensions.Http;
+using Polly.Retry;
 
 namespace NSE.WebApp.MVC.Configuration;
 
@@ -13,7 +16,12 @@ public static class DependencyInjectionConfig
         services.AddHttpClient<IAutenticacaoService, AutenticacaoService>();
         
         services.AddHttpClient<ICatalogoService, CatalogoService>()
-            .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>();
+            .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>()
+            .AddPolicyHandler(PollyExtensions.EsperarTentar())
+            .AddTransientHttpErrorPolicy(p=> 
+                p.CircuitBreakerAsync(
+                    5, 
+                    TimeSpan.FromSeconds(30)));
         
         #region Refit
         // services.AddHttpClient("Refit",
@@ -27,5 +35,27 @@ public static class DependencyInjectionConfig
         
         services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
         services.AddScoped<IUser, AspNetUser>();
+    }
+}
+
+public class PollyExtensions
+{
+    public static AsyncRetryPolicy<HttpResponseMessage> EsperarTentar()
+    {
+        var retry =  HttpPolicyExtensions
+            .HandleTransientHttpError()
+            .WaitAndRetryAsync(new[]
+            {
+                TimeSpan.FromSeconds(1),
+                TimeSpan.FromSeconds(5),
+                TimeSpan.FromSeconds(10),
+            }, (outcome, timespan, retryCount, context) =>
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine($"Tentando pela {retryCount} vez!");
+                Console.ForegroundColor = ConsoleColor.White;
+            });
+
+        return retry;
     }
 }
--- NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Controllers/HomeController.cs
+++ NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Controllers/HomeController.cs
@@ -4,23 +4,36 @@ using NSE.WebApp.MVC.Models;
 
 namespace NSE.WebApp.MVC.Controllers;
 
-public class HomeController : Controller
+public class HomeController : MainController
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    // private readonly ILogger<HomeController> _logger;
+    //
+    // public HomeController(ILogger<HomeController> logger)
+    // {
+    //     _logger = logger;
+    // }
+    //
+    // public IActionResult Index()
+    // {
+    //     return View();
+    // }
+    //
+    // public IActionResult Privacy()
+    // {
+    //     return View();
+    // }
+    
+    [Route("sistema-indisponivel")]
+    public IActionResult SistemaIndisponivel()
     {
-        _logger = logger;
-    }
+        var modelErro = new ErrorViewModel
+        {
+            Mensagem = "O sistema está temporariamente indisponível, isto pode ocorrer em momentos de sobrecarga de usuários.",
+            Titulo = "Sistema indisponível.",
+            ErroCode = 500
+        };
 
-    public IActionResult Index()
-    {
-        return View();
-    }
-
-    public IActionResult Privacy()
-    {
-        return View();
+        return View("Error", modelErro);
     }
 
     [Route("erro/{id:length(3,3)}")]
